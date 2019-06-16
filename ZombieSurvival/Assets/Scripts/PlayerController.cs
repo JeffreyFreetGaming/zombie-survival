0f3add785bd8953ed306722f76127d4bd0237ffe
Jeffrey Freet <jeffreyfreet@yahoo.com>
2019-06-16 11:01:50 -0400

    Simple Modeling + Player Controller
    Simples models created to work with
    Player Controller created

---
+++ ZombieSurvival/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //locks cursor to game window
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float strafe = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+    }
+}
