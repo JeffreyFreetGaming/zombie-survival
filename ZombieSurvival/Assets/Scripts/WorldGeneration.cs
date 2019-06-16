fde9fc8ad17555c32ab230c77a77e22f4214e16f
Jeffrey Freet <jeffreyfreet@yahoo.com>
2019-06-16 16:14:50 -0400

    Created some MAts

---
+++ ZombieSurvival/Assets/Scripts/WorldGeneration.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGeneration : MonoBehaviour
+{
+
+    public GameObject tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numTrees = Random.Range(50, 75);
+
+        for(int i = 0; i < numTrees; i++)
+        {
+            Instantiate(tree, new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50)), Quaternion.identity);
+        }
+    }
+}
